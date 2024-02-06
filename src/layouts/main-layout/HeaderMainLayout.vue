60371a7c7dff2902b31201edbae538bb4e6d8ec5
Hung Anh <anhlhpl52119@gmail.com>
2024-02-06 21:36:45 +0700

    style: add border

--- src/layouts/main-layout/HeaderMainLayout.vue
+++ src/layouts/main-layout/HeaderMainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-16 px-8 flex-btw-center b b-b-abd">
+  <div class="py-16 px-8 flex-btw-center b-b  b-b-abd">
     <div class="md:hidden block">
       <i class="i-mingcute:menu-fill block text-30" @click="isOpen = true" />
       <CollapsedMenu v-model:open="isOpen" />
