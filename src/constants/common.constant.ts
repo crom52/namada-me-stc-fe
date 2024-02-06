2d91e2bee2422c089652df9bc2eeee92fa1cb55b
Hung Anh <anhlhpl52119@gmail.com>
2024-02-06 21:36:11 +0700

    feat: chrome extension pack

--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_CACHE_TIME = 60 * 60 * 24 * 7; // 7 days
 export const UNHANDLED_SERVER_ERROR = 'error'; // 7 days
+export const NAMADA_CHROME_EXTENSION_ID = 'hnebcbhjpeejiclgbohcijljcnjdofek';
