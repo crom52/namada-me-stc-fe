b0d4a6c0d921a68ce62bd0257172da8d240de38b
Hung Anh <anhlhpl52119@gmail.com>
2024-02-08 23:35:34 +0700

    feat: dynamic tag

---
+++ src/components/common/DynamicStatusTag.vue
@@ -0,0 +1,92 @@
+<template>
+  <ATag :color="dynamicTag.color" :bordered="!noBorder" class="rounded-7">
+    <div v-if="dynamicTag.icon" class="flex gap-5 items-center">
+      <i class="text-16 inline-block" :class="dynamicTag.icon" />
+      <span>
+        {{ dynamicTag.statusText }}
+      </span>
+    </div>
+    <span v-else>{{ dynamicTag.statusText }}</span>
+  </ATag>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  status: OrNullish<string>
+  loading?: boolean
+  noBorder?: boolean
+}>();
+
+const { status, loading } = toRefs(props);
+
+const statusColorMap: Record<string, string[]> = {
+  default: ['cancelled'], // plain
+  success: ['done', 'full_synced', 'sync_failed', 'synced'], // green
+  purple: ['pending', 'webhook_not_enough'], // bg: #f9f0ff || border: #d3adf7
+  error: ['in-active', 'loser'], // red
+  processing: ['processing'], // yellow
+  geekblue: ['not_config', 'developer'], // bg: #f0f5ff || border: #adc6ff
+  cyan: ['ready_to_sync', 'active'], // bg: #e6fffb || border: #87e8de
+  magenta: ['user', 'manager'], // bg: #fff0f6 || border: #ffadd2
+};
+
+const statusTextMap: Record<string, string> = {
+  cancelled: 'Đã hủy',
+  success: 'Thành công',
+  pending: 'Đang chờ',
+  done: 'Hoàn tất',
+  not_config: 'Chưa config',
+  full_synced: 'Full sync',
+  ready_to_sync: 'Sẵn sàng sync',
+  sync_failed: 'Sync thất bại',
+  webhook_not_enough: 'Webhook không đủ',
+  synced: 'Đã sync',
+  user: 'Nhân viên',
+  processing: 'Đang tải',
+  active: 'Đang hoạt động',
+  manager: 'Quản lý',
+};
+
+const statusIconMap: Record<string, string> = {
+  'active': 'i-material-symbols:verified-user-rounded',
+  'manager': 'i-material-symbols:verified-user-rounded',
+  'in-active': 'i-fluent:plug-disconnected-20-filled',
+  'unknown': 'i-pajamas:severity-unknown',
+  'processing;': 'i-svg-spinners:tadpole',
+};
+
+const findColor = (status: OrNullish<string>) => {
+  if (false || loading.value) {
+    return 'processing';
+  }
+  if (!status) {
+    return 'default';
+  }
+
+  for (const [color, statusList] of Object.entries(statusColorMap)) {
+    if (statusList.includes(status)) {
+      return color;
+    }
+  }
+};
+
+const findLabel = (status: OrNullish<string>) => {
+  if (false || loading.value) {
+    return 'Đang tải';
+  }
+  if (status) {
+    return statusTextMap[status] ?? status.toUpperCase();
+  }
+
+  return 'UNKNOWN';
+};
+
+const dynamicTag = computed(() => {
+  const statusText = loading.value ? 'processing' : status.value!;
+  return {
+    statusText: findLabel(statusText),
+    color: findColor(statusText),
+    icon: false ? 'i-svg-spinners:tadpole' : statusIconMap[statusText],
+  };
+});
+</script>
---
+++ src/components/table/MyValidatorsTable.vue
@@ -0,0 +1,93 @@
+<template>
+  <ATable :dataSource="data">
+    <ATableColumn
+      key="validator"
+      :ellipsis="true"
+      :width="250"
+      fixed="left"
+    >
+      <template #title>
+        <span>Validator</span>
+      </template>
+
+      <template #default="{ record }: {record: any}">
+        <AButton type="link" class="p0">
+          {{ record.name }}
+        </AButton>
+      </template>
+    </ATableColumn>
+    <ATableColumn
+      key="status"
+    >
+      <template #title>
+        <span>Status</span>
+      </template>
+
+      <template #default="{ record }: {record: any}">
+        <template v-for="tag in record.tags" :key="tag">
+          <DynamicStatusTag :status="tag" />
+        </template>
+      </template>
+    </ATableColumn>
+    <ATableColumn
+      key="amount"
+    >
+      <template #title>
+        <span>Staked Amount</span>
+      </template>
+
+      <template #default="{ record }: {record: any}">
+        123123123
+      </template>
+    </ATableColumn>
+    <ATableColumn
+      key="un-bonded-amount"
+    >
+      <template #title>
+        <span>Unbonded Amount</span>
+      </template>
+
+      <template #default="{ record }: {record: any }">
+        1231231231231231
+      </template>
+    </ATableColumn>
+    <ATableColumn
+      key="withdrawable-amount"
+    >
+      <template #title>
+        <span> Withdrawable Amount</span>
+      </template>
+
+      <template #default="{ record }: {record: any}">
+        1231231231231231
+      </template>
+    </ATableColumn>
+  </ATable>
+</template>
+
+<script lang="ts" setup>
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+</script>
