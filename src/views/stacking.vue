a65c8048921c96a532462f57075b6c2f87ef5818
Hung Anh <anhlhpl52119@gmail.com>
2024-03-01 22:45:20 +0700

    chore: update

--- src/views/stacking.vue
+++ src/views/stacking.vue
@@ -1,9 +1,14 @@
-<template>
-  <div class="page">
-    Stacking
-  </div>
-</template>
 
 <script lang="ts" setup>
-
+import { DownOutlined, SmileOutlined } from '@ant-design/icons-vue';
 </script>
+<template>
+  <div class="page">
+    <div>
+      <p>
+        My Validators
+      </p>
+      <MyValidatorsTable class="mt-16" />
+    </div>
+  </div>
+</template>
